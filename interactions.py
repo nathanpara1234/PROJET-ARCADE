c195f22bc9e95e845145c0203618998ce9e0179d
Nathan Para <nathan.para@epfl.ch>
2026-05-27 23:38:23 +0200

    aaa

--- interactions.py
+++ interactions.py
@@ -1,9 +1,10 @@
 from math import sqrt
 import arcade
-
+from textures import TEXTURE_EMPTY_CHEST
 from constants import SPIKES_SWITCH_TIME
 from player import Player
 from weapons import Boomerang, Sword, BoomerangState
+from text import Text
 
 
 def should_restart_after_collision(
@@ -54,3 +55,39 @@ def should_restart_after_spikes_collision(
     if not spikes_are_active:
         return False
     return bool(arcade.check_for_collision_with_list(player, spikes))
+
+def update_collectibles(# fonction de collision avec item recuperable
+    player: Player,
+    crystals: arcade.SpriteList,
+    crystal_sound: arcade.Sound,
+    keys: arcade.SpriteList,
+    keys_sound: arcade.Sound,
+    chests: arcade.SpriteList,
+    chests_sound: arcade.Sound,
+    text: Text,
+) -> None:
+    collision_crystals = arcade.check_for_collision_with_list(player, crystals)
+    for crystal in collision_crystals:
+
+        crystal.remove_from_sprite_lists()
+        arcade.play_sound(crystal_sound)
+        player.score += 1
+        text.update_score(player)
+        if player.score >= text.total_crystals:
+            text.show_end()
+
+    collision_keys = arcade.check_for_collision_with_list(player, keys)
+    for key in collision_keys:
+        key.remove_from_sprite_lists()
+        arcade.play_sound(keys_sound)
+        player.key += 1
+        text.update_keys(player)
+
+    collision_chests = arcade.check_for_collision_with_list(player, chests)
+    for chest in collision_chests:
+        if player.key > 0:
+            chest.texture = TEXTURE_EMPTY_CHEST
+            arcade.play_sound(chests_sound)
+            player.key -= 1
+            text.update_keys(player)
+            player.player_become_indestructible()
